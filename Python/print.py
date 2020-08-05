74966ade01d307ecaf1e62773fdd0e6782740126
Harshit <harshitchopra0712@gmail.com>
2020-08-05 23:33:16 +0530

    Basic print function in python

---
+++ Python/print.py
@@ -0,0 +1,21 @@
+# Basic 'print'
+print("Hello, Monty")
+print("Python")
+print('Learning Python...')
+# print("Same line") print("Same line")  ERROR
+# Arguments
+print("Hello", "Monty", "Python")
+print('Hello', "Again")
+# Escape sequences
+print("Hello Monty\nPython")
+print("Hello Monty\\Python")
+# Keywords
+# end
+print("Hello", "Monty", end=" ")
+print("Python")
+print("Hello Monty", end="@")
+print("Python")
+# sep
+print("Hello", "Monty", sep="-", end=" ")
+print("Python")
+# print("Hello Monty ", sep="@") NOT WORKING
