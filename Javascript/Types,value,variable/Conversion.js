411f7c4b9f346b8675c774617b8f844a666f711d
Harshit <harshitchopra0712@gmail.com>
2020-08-13 23:12:56 +0530

    Conversion

---
+++ Javascript/Types,value,variable/Conversion.js
@@ -0,0 +1,16 @@
+//false values
+console.log(0==false);
+console.log(-0==false);
+console.log(""==false);
+console.log(undefined);
+console.log(null);
+console.log((0/0))
+//Conversion
+//String to number
+let num1 = "30";    //cannot use + operator
+let num2 = "40"; 
+console.log(num1-num2);
+//Number to string
+console.log(typeof (num1+""));
+
+
